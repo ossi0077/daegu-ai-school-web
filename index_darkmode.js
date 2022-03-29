4d592dc5634e41f136c798c15f17a137952a6db8
double_S <ossi0077@naver.com>
2022-03-29 17:37:00 +0900

    커밋

--- index_darkmode.js
+++ index_darkmode.js
@@ -1,6 +1,8 @@
 //다크모드 낮모드 함수
 function dark_mode(){
     document.getElementById('indexstyle').href='index_dark_style.css';
+    document.querySelector('body').style.backgroundColor='black';
+    document.querySelector('body').style.color='white';
     let hyper = document.querySelectorAll('a');
     for(i=0;i<hyper.length;i++){
         hyper[i].style.color='white'
@@ -9,6 +11,8 @@ function dark_mode(){
 
 function day_mode(){
     document.getElementById('indexstyle').href='index_style.css';
+    document.querySelector('body').style.backgroundColor='inherit';
+    document.querySelector('body').style.color='inherit';
     let hyper = document.querySelectorAll('a');
     for(i=0;i<hyper.length;i++){
         hyper[i].style.color='inherit'
